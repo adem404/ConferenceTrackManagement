50a817cb9d77621a45daf3a1c29d553a83bd1831
Adem Dagdeviren <adem.dagdeviren@outlook.de>
2022-02-11 18:46:45 +0100

    test successful

--- de.itemis.codingchallenge/src/de/itemis/codingchallenge/test/ConferenceTrackManagementTest.java
+++ de.itemis.codingchallenge/src/de/itemis/codingchallenge/test/ConferenceTrackManagementTest.java
@@ -1,12 +1,14 @@
 package de.itemis.codingchallenge.test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.apache.commons.io.FileUtils;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.Test;
 import de.itemis.codingchallenge.Conference;
 import de.itemis.codingchallenge.ConferenceTrackGenerator;
@@ -20,13 +22,11 @@ class ConferenceTrackManagementTest {
 
 		BufferedReader reader = new BufferedReader(new FileReader(input.toString()));
 		Conference conference = new ConferenceTrackGenerator().generator(reader);
-
-		File conferenceOutput = new File("resources/conference.txt");
-		File expectedOutput = new File("resources/original_output.txt");
-
-		FileUtils.writeStringToFile(conferenceOutput, conference.toString());
-
-		assertTrue(FileUtils.contentEquals(conferenceOutput, expectedOutput));
+		Path conferenceOutputPath = Paths.get("resources/conference.txt");
+		Path expectedOutputPath = Paths.get("resources/expected_output.txt");
+		Files.writeString(conferenceOutputPath, conference.toString(), StandardCharsets.UTF_8);
+		long mismatch = Files.mismatch(conferenceOutputPath, expectedOutputPath);
+		assertTrue(mismatch == -1);
 	}
 
 }
