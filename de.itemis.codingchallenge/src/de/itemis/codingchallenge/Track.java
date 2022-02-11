1fdea8f13d42fed61c3f156f9d3c245178b5bd60
Adem Dagdeviren <adem.dagdeviren@outlook.de>
2022-02-11 17:10:56 +0100

    override toString()

--- de.itemis.codingchallenge/src/de/itemis/codingchallenge/Track.java
+++ de.itemis.codingchallenge/src/de/itemis/codingchallenge/Track.java
@@ -14,5 +14,13 @@ public class Track {
 	public void addSessionToTrack(Session session) {
 		sessionList.add(session);
 	}
-
+	
+	@Override
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		for (int i = 0; i < sessionList.size(); i++) {
+			s.append(sessionList.get(i));
+		}
+		return s.toString();
+	}
 }
